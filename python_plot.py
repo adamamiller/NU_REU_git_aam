babaf9d3fddcc4d26a277ecc662da6bdcff49639
Adam Miller <amiller.astro@gmail.com>
2019-07-08 13:49:32 -0500

    initial commit

---
+++ python_plot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,2*np.pi,1000)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+fig.tight_layout()
